6f3ef5b69719ce1e5edc26c1bcdfd2f4207c8db8
Feras <feras.alawadi@zarcony.com>
2020-07-16 11:56:40 +0300

    init repo

---
+++ app.py
@@ -0,0 +1,21 @@
+# 1 2 ,3 ,4 ,5, 6,10, -1 , -2 ,-4 , -5x  => Integers. اعداد صحيحة
+# 1.3, 5.6, 9.32 ,3.3333     => Floating Point (double) اعداد عشرية
+# A,B,C,D,E,F,G,H,I,J...Z,+,-,#,$,%,&,~./,":  | \ ; ==> Characters. محارف
+# hello World, Malek went to School, Malek Loves Programming languages, Malek Likes Shawarma. ==> Strings السلاسل
+
+# todo
+#  Learn Storing Data Values, Bit,Byte,MB,GB ...etc.
+
+char = 'A'
+char1 = '+'
+name = "Malek"
+blah = 10
+x2 = 2.5
+
+print("char: " + char)
+print("char1: " + char1)
+print('String: ' + name)
+print("Integer: ")
+print(blah)
+print("Float: ")
+print(x2)

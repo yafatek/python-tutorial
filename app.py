88b993a085dfc9cdd48cc5a26c99f8ab2ed0d65f
Feras <feras.alawadi@zarcony.com>
2020-07-18 18:07:59 +0300

    lists-temp

--- app.py
+++ app.py
@@ -16,10 +16,35 @@ name = "Malek"
 blah = 10
 x2 = 2.5
 
-print("char: " + char)
-print("char1: " + char1)
-print('String: ' + name)
-print("Integer: ")
-print(blah)
-print("Float: ")
-print(x2)
+# print("char: " + char)
+# print("char1: " + char1)
+# print('String: ' + name)
+# print("Integer: ")
+# print(blah)
+# print("Float: ")
+# print(x2)
+
+# Number Types in Python
+# there is 4 Types in Python.
+int_num = 10  # Integer Value  LessThan 1000000, Greater than -9999991
+float_num = 10.2  # Float Value
+complex_num = 3.14j  # Complex. => pie 3.1476323899
+long_num = 8538765433456789
+
+# 1, 2, 3, 4, 5, 6, 7
+nums = [1, 2, 3, 4, 5, 6, 7]
+
+print(nums[3])
+
+# List Type. arrays index always start from ZERO (0).  00000000000
+# 0 , 1 ,2, 3
+list = [123, 'abc', 10.2, 'd']
+print(list[2])
+
+list1 = ['hello', 'world']
+
+# print(list)
+# print first two elements.
+# print(list[0:2])
+# Duplicate List
+# print(list1 * 2)
